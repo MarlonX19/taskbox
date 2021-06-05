ab2572b3f02ae452bcf21a508c512a37f2428829
marlonx19 <marlon.englemam@gmail.com>
2021-06-05 16:15:03 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import './App.css';
+import { Button } from './stories/Button';
+
+function App() {
+
+  const onClick = () => {
+    alert('aquiii')
+  }
+
+  const onMouseLeave = () => {
+    alert('onMouseLeave')
+  }
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', background: '#fff' }}>
+      <Button
+        primary={true}
+        label='Procurar voo'
+        size='large'
+        onClick={onClick}
+        onMouseLeave={onMouseLeave}
+      />
+    </div>
+  );
+}
+
+export default App;
